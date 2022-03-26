debd1566129c9b9f8d14ee64aebfc1c4916e50a9
cjsmocjsmo <porthose.cjsmo.cjsmo@gmail.com>
2022-03-25 23:18:40 -0700

    update

--- ATSBuild.sh
+++ ATSBuild.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm /home/charliepi/go/atsGoStrip/assets/index.html;
 rm /home/charliepi/go/atsGoStrip/assets/*.js;
-rm /home/charliepi/go/atsGoStrip/assets/*.css;
+rm /home/charliepi/go/atsGoStrip/assets/index*.css;
 rm -rf /home/charliepi/go/atsGoStrip/assets/images
 
 cd /home/charliepi/astro/AlphaTreeService;
@@ -10,5 +10,5 @@ mv /home/charliepi/astro/AlphaTreeService/dist/index.html /home/charliepi/go/ats
 mv /home/charliepi/astro/AlphaTreeService/dist/assets/*.js /home/charliepi/go/atsGoStrip/assets/;
 mv /home/charliepi/astro/AlphaTreeService/dist/assets/*.css /home/charliepi/go/atsGoStrip/assets/;
 mv /home/charliepi/astro/AlphaTreeService/dist/assets/images /home/charliepi/go/atsGoStrip/assets/;
-cd /home/charliepi/go/atsGoStrip;
-docker-compose up -d --build;
+# cd /home/charliepi/go/atsGoStrip;
+# docker-compose up -d --build;
