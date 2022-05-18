2222619ffda4a457c06f5cbffc0fab6b0c9c2da3
cjsmocjsmo <porthose.cjsmo.cjsmo@gmail.com>
2022-05-18 09:22:34 -0700

    dont use cert

--- atsGo.go
+++ atsGo.go
@@ -293,14 +293,14 @@ func main() {
 	// }
 	// ss.ListenAndServe()
 
-	// http.ListenAndServe(":80", handlers.CORS(handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}),
-	// 	handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"}),
-	// 	handlers.AllowedOrigins([]string{"*"}))(r))
+	http.ListenAndServe(":80", handlers.CORS(handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}),
+		handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"}),
+		handlers.AllowedOrigins([]string{"*"}))(r))
 
-	http.ListenAndServeTLS(":80", "/root/atsio.crt", "/root/atsio.key",
-		handlers.CORS(handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}),
-			handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"}),
-			handlers.AllowedOrigins([]string{"*"}))(r))
+	// http.ListenAndServeTLS(":80", "/root/atsio.crt", "/root/atsio.key",
+	// 	handlers.CORS(handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}),
+	// 		handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"}),
+	// 		handlers.AllowedOrigins([]string{"*"}))(r))
 
 }
 
