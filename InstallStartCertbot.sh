3fe5edd2a3e60066724906a86cc65c033d107c05
cjsmocjsmo <porthose.cjsmo.cjsmo@gmail.com>
2022-05-21 07:09:47 -0700

    fix install

--- InstallStartCertbot.sh
+++ InstallStartCertbot.sh
@@ -11,9 +11,9 @@ sudo snap install core;
 sudo snap refresh core;
 sudo snap install --classic certbot;
 sudo ln -s /snap/bin/certbot /usr/bin/certbot;
-sudo mkdir /root/data
-sudo certbot certonly -v --standalone -d atsio.xyz \
-    --cert-path /root/data --key-path /root/data --fullchain-path /root/data --chain-path /root/data;
+sudo certbot certonly -v --standalone -d atsio.xyz;
+
+    # --cert-path /root/data --key-path /root/data --fullchain-path /root/data --chain-path /root/data;
 
 # certbot certonly --dns-google -d atsio.xyz;
 
