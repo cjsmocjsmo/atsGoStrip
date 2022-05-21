53b685a7e1d517510ab8c8eecbed61159f128c41
cjsmocjsmo <porthose.cjsmo.cjsmo@gmail.com>
2022-05-21 07:02:06 -0700

    update install script

--- InstallStartCertbot.sh
+++ InstallStartCertbot.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update;
 sudo apt-get -y dist-upgrade;
-sudo apt-get -y install golang git;
+sudo apt-get -y install golang;
 sudo apt-get -y autoclean;
 sudo apt-get -y autoremove;
 
@@ -11,7 +11,8 @@ sudo snap install core;
 sudo snap refresh core;
 sudo snap install --classic certbot;
 sudo ln -s /snap/bin/certbot /usr/bin/certbot;
-sudo certbot certonly --standalone -d atsio.xyz \
+sudo mkdir /root/data
+sudo certbot certonly -v --standalone -d atsio.xyz \
     --cert-path /root/data --key-path /root/data --fullchain-path /root/data --chain-path /root/data;
 
 # certbot certonly --dns-google -d atsio.xyz;
